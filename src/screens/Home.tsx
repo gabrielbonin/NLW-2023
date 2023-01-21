459fe480abd3ef08971d8147611b0125799868d3
Gabriel Bonin <gabriel.bonin@mblabs.com.br>
2023-01-21 18:37:10 -0300

    fix home

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -36,20 +36,20 @@ export function Home() {
           {datesFromYearBeginning.map((date, index) => (
             <HabitDay key={date.toISOString()} />
           ))}
+          {amountOfDaysToFill > 0 &&
+            Array.from({
+              length: amountOfDaysToFill,
+            }).map((_, index) => (
+              <View
+                key={index}
+                className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800 opacity-40"
+                style={{
+                  width: DAY_SIZE,
+                  height: DAY_SIZE,
+                }}
+              ></View>
+            ))}
         </View>
-        {amountOfDaysToFill > 0 &&
-          Array.from({
-            length: amountOfDaysToFill,
-          }).map((_, index) => (
-            <View
-              key={index}
-              className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800 opacity-40"
-              style={{
-                width: DAY_SIZE,
-                height: DAY_SIZE,
-              }}
-            ></View>
-          ))}
       </ScrollView>
     </View>
   );
